34cb8a64590b5f820aab652abc88435cdd2236c2
Tamal <tsarker181054@bscse.uiu.ac.bd>
2022-01-08 11:29:19 +0600

    Convert to Singleton

--- MyFancyJavaProject/src/myfancyjavaproject/MyFancyJavaProject.java
+++ MyFancyJavaProject/src/myfancyjavaproject/MyFancyJavaProject.java
@@ -1,13 +1,13 @@
 
 package myfancyjavaproject;
 
-public class MyFancyJavaProject {
-    
-    
+private class MyFancyJavaProject {
+
+    private static MyFancyJavaProject student;
     int roll;
     int age;
     
-    MyFancyJavaProject(int roll, int age){
+    private MyFancyJavaProject(int roll, int age){
         
         this.roll = roll;
         this.age = age;
@@ -16,12 +16,24 @@ public class MyFancyJavaProject {
     
     
   
-    public static void main(String[] args) {
-        
-        MyFancyJavaProject student = new MyFancyJavaProject(12,15);
-        
-        System.out.println("roll:"+student.roll+"__________"+"age:"+student.age);
-       
+//    public static void main(String[] args) {
+//
+//        //MyFancyJavaProject student = new MyFancyJavaProject(12,15);
+//
+//        //System.out.println("roll:"+student.roll+"__________"+"age:"+student.age);
+//
+//
+//
+//    }
+
+    private static get student(int roll,int age)
+    {
+        if (student==NULL)
+        {
+            student=new MyFancyJavaProject(roll,age);
+        }
+        return student();
+
     }
     
 }
